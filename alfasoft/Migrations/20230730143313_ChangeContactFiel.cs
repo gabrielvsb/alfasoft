ce4169f6ec140086cb9cb68aed3345733afad34e
gabrielvsb <gabriel.vitorsb323@gmail.com>
2023-07-30 11:35:04 -0300

    update migrations

---
+++ alfasoft/Migrations/20230730143313_ChangeContactFiel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alfasoft.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeContactFiel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
