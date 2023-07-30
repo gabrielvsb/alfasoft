26dca093c158c250ce0ab6a198e59c878f48e4d3
gabrielvsb <gabriel.vitorsb323@gmail.com>
2023-07-30 11:34:56 -0300

    update initial controller

--- alfasoft/Program.cs
+++ alfasoft/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Contact}/{action=Index}");
 
 app.Run();
